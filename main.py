abd54c33ff233d8e8558884ea3c571919dd580ed
Miguel Raposo <migpovrap@hotmail.com>
2025-01-04 14:43:32 +0000

    Improved code, mooshak 900 1 timelimit 15 Wrong 18 Accepted

--- main.py
+++ main.py
@@ -1,70 +1,85 @@
 #pylint: skip-file
+import sys
 import pulp
 from pulp import PULP_CBC_CMD
 
 def main():
-    n, m, t = map(int, input().split()) # n - num factories, m - num countries, t - num children
+    data = sys.stdin.read().splitlines()
+    
+    n, m, t = map(int, data[0].split()) # n - num factories, m - num countries, t - num children
+
+    # Used to check if the problem has a solution
+    total_stock = total_min_req = 0
+    country_stock = [0] * (m + 1)
 
     # Parse info regarding each of the n factories
-    factories = []
-    for _ in range(n):
-        factory_info = list(map(int, input().split()))
-        factories.append(factory_info) # [id_fab, id_country, stock]
+    factories = {}
+    for i in range(1, n + 1):
+        factory_info = list(map(int, data[i].split()))
+        if factory_info[2] > 0: # Only if the factory has stock > 0
+            factories[factory_info[0]] = factory_info # {id_fab: [id_fab, id_country, stock]}
+            country_stock[factory_info[1]] += factory_info[2]
+            total_stock += factory_info[2]
 
     # Parse info regarding each of the m countries
-    countries = []
-    for _ in range(m):
-        country_info = list(map(int, input().split()))
-        countries.append(country_info) # [id_country, export_limit, min_toys_required]
+    countries = {}
+    for i in range(n + 1, n + m + 1):
+        country_info = list(map(int, data[i].split()))
+        if country_info[1] > 0 and country_info[2] > 0:  # Only if the country has an export limit > 0 and min_toys_required > 0
+            countries[country_info[0]] = country_info # {id_country: [id_country, export_limit, min_toys_required]}
+            total_min_req += country_info[2]
+
+    if total_stock < total_min_req: # Early exit if the problem can't be solved
+        print(-1)
+        return
 
     # Parse the requests of t children
-    requests = []
-    for _ in range(t):
-        request_info = list(map(int, input().split()))
-        requests.append(request_info) # [id_child, id_country, fab_ids, ...]
+    requests = []    
+    for i in range(n + m + 1, n + m + t + 1):
+        request_info = list(map(int, data[i].split()))
+        valid_factories = [factory for factory in request_info[2:] if factory in factories]
+        if valid_factories: # Only consider requests with valid factories
+            requests.append([request_info[0], request_info[1], valid_factories]) # [id_child, id_country, fab_ids]
+
+    n = len(factories)
+    m = len(countries)
+    t = len(requests)
+
+    factories_per_country = {j: set(i for i in factories.keys() if factories[i][1] == j) for j in countries.keys()} # Factories for each country {country_id: {factory_ids}}
+    requests_per_country = {j: set(k for k in range(t) if requests[k][1] == j) for j in countries.keys()} # Request indices for each country {country_id: {request_indices}}
+    valid_factories = {k: set(requests[k][2]) for k in range(t)} # Ensure that children only receive gifts from requested factories
 
     problem = pulp.LpProblem(sense=pulp.LpMaximize)
     
     # Binary decision variables: happy[k][i] is 1 if child k receives a gift from factory i, 0 otherwise
-    happy = pulp.LpVariable.dicts("happy", (range(t), range(n)), cat='Binary')
+    happy = pulp.LpVariable.dicts("happy", ((k, i) for k in range(t) for i in valid_factories[k]), cat='Binary')
 
     # Maximize the number of fulfilled requests
-    problem += pulp.lpSum(happy[k][i] for k in range(t) for i in range(n))
+    problem += pulp.lpSum(happy[k,i] for k in range(t) for i in valid_factories[k])
 
     # Constraints
     # Each child can receive at most one gift
     for k in range(t):
-        problem += pulp.lpSum(happy[k][i] for i in range(n)) <= 1
+        problem += pulp.lpSum(happy[k,i] for i in valid_factories[k]) <= 1
 
     # A factory cannot give more gifts than it has in stock
-    for i in range(n):
-        problem += pulp.lpSum(happy[k][i] for k in range(t)) <= factories[i][2]
+    for i in factories.keys():
+        problem += pulp.lpSum(happy[k,i] for k in range(t) if i in valid_factories[k]) <= factories[i][2]
 
     # A country cannot export more gifts than its export limit
-    for j in range(m):
-        problem += pulp.lpSum(happy[k][i] for k in range(t) for i in range(n) if factories[i][1] == j + 1) <= countries[j][1]
+    for j in countries.keys():
+        problem += pulp.lpSum(happy[k,i] for k in range(t) for i in factories_per_country[j] if i in valid_factories[k]) <= countries[j][1]
+        
+        # Each country must receive at least its minimum required gifts
+        problem += pulp.lpSum(happy[k,i] for k in requests_per_country[j] for i in valid_factories[k]) >= countries[j][2]
 
-    # Each country must receive at least its minimum required gifts
-    for j in range(m):
-        problem += pulp.lpSum(happy[k][i] for k in range(t) if requests[k][1] == j + 1 for i in range(n)) >= countries[j][2]
-
-    # Ensure that children only receive gifts from requested factories
-    for k in range(t):
-        for i in range(n):
-            if i + 1 not in requests[k][2:]:
-                problem += happy[k][i] == 0
-
-    # Solve the problem
-    problem.solve(PULP_CBC_CMD(msg=False))
-
-    # Check if the problem has an optimal solution
-    if pulp.LpStatus[problem.status] != 'Optimal':
+    # Solve the problem and check if it has an optimal solution
+    if problem.solve(PULP_CBC_CMD(msg=False, threads=2, warmStart=True)) != pulp.LpStatusOptimal:
         print(-1)
         return
 
-    # Calculate the number of satisfied requests
-    satisfied_requests = sum(1 for k in range(t) for i in range(n) if pulp.value(happy[k][i]) == 1)
-    print(satisfied_requests)
+    # Get the number of satisfied requests
+    print(int(pulp.value(problem.objective)))
 
 if __name__ == "__main__":
     main()
