9dcae05f42aed14f6ea71bc3bec62706e3d06cea
Inês Costa <94355111+inesiscosta@users.noreply.github.com>
2025-01-09 02:54:36 +0000

    Reduce complexity.

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ class Country:
     self.id = id
     self.export_limit = export_limit
     self.min_gifts = min_gifts
-    self.children = []
     self.exports = []
+    self.chosen_factories = []
+    self.num_country_requests = 0
 
 class Child:
   def __init__(self, id, factories):
@@ -48,16 +49,17 @@ def main():
     child_id, country_id, *requested_factories = map(int, data[line].split())
     valid_factories = [factories[factory_id] for factory_id in requested_factories if factory_id in factories]
     children[child_id] = Child(child_id, valid_factories)
-    countries[country_id].children.append(children[child_id])
     for factory_id in requested_factories:
+      countries[country_id].chosen_factories.append((child_id, factory_id))
       if factories[factory_id].country.id != country_id:
         countries[factories[factory_id].country.id].exports.append((children[child_id].id, factory_id))
       factories[factory_id].requests.append(children[child_id])
+      countries[country_id].num_country_requests += 1
 
   # Create LP problem
   problem = LpProblem(sense=LpMaximize)
 
-  happy = LpVariable.dicts("happy", [(child.id, factory.id) for child in children.values() for factory in child.factories], lowBound=0, upBound=1, cat='Binary')
+  happy = {(child.id, factory.id): LpVariable(f"happy_{child.id}_{factory.id}", cat='Binary') for child in children.values() for factory in child.factories}
 
   problem += lpSum(happy[child.id, factory.id] for child in children.values() for factory in child.factories)
 
@@ -73,10 +75,10 @@ def main():
 
   for country in countries.values():
     # A country must receive at least its minimum number of gifts.
-    problem += lpSum(happy[child.id, factory.id] for child in country.children for factory in child.factories) >= country.min_gifts
+    problem += lpSum(happy[child_id, factory_id] for (child_id, factory_id) in country.chosen_factories) >= country.min_gifts
 
     # A country cannot export more gifts than its export limit.
-    problem += lpSum(happy[id_child, id_factory] for (id_child, id_factory) in country.exports) <= country.export_limit
+    problem += lpSum(happy[child_id, factory_id] for (child_id, factory_id) in country.exports) <= country.export_limit
 
   # Solve the problem and check if it has an optimal solution.
   if problem.solve(GLPK(msg=False)) == LpStatusOptimal:
