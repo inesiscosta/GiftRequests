75785bac8c940829df3c8d054c075c5c7f854b8f
Inês Costa <94355111+inesiscosta@users.noreply.github.com>
2025-01-10 13:00:09 +0000

    Refactor.

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def main():
     for line in range(1, num_factories + 1):
         factory_id, country_id, stock = map(int, data[line].split())
         if stock > 0:
-            factories[factory_id] = {'id': factory_id,
-            'country_id': country_id, 'stock': stock, 'requests': []}
+            factories[factory_id] = {'country_id': country_id,
+            'stock': stock, 'requests': []}
 
     for line in range(num_factories + 1, num_factories + num_countries + 1):
         country_id, export_limit, min_gifts = map(int, data[line].split())
@@ -28,33 +28,35 @@ def main():
     for line in range(num_factories + num_countries + 1,
     num_factories + num_countries + num_children + 1):
         child_id, country_id, *requested_factories = map(int, data[line].split())
-        valid_factories = [factories[factory_id] for factory_id in
-        requested_factories if factory_id in factories]
+        valid_factories = []
+        for factory_id in requested_factories:
+            if factory_id in factories:
+                factory = factories[factory_id]
+                valid_factories.append(factory_id)
+                countries[country_id]['chosen_factories'].append((child_id, factory_id))
+                if factory['country_id'] != country_id:
+                    countries[factory['country_id']]['exports'].append((child_id, factory_id))
+                factory['requests'].append(child_id)
         children[child_id] = {'id': child_id, 'factories': valid_factories}
-        for factory in valid_factories:
-            countries[country_id]['chosen_factories'].append((child_id, factory['id']))
-            if factory['country_id'] != country_id:
-                countries[factory['country_id']]['exports'].append((child_id, factory['id']))
-            factory['requests'].append(child_id)
 
     # Create LP problem
     problem = LpProblem(sense=LpMaximize)
-    happy = LpVariable.dicts("happy", ((child_id, factory['id']) for
-    child_id, child in children.items() for factory in child['factories']), cat='Binary')
+    happy = LpVariable.dicts("happy", ((child_id, factory_id) for
+    child_id, child in children.items() for factory_id in child['factories']), cat='Binary')
 
-    problem += lpSum(happy[child_id, factory['id']] for child_id, child
-    in children.items() for factory in child['factories'])
+    problem += lpSum(happy[child_id, factory_id] for child_id, child
+    in children.items() for factory_id in child['factories'])
 
     # Constraints
 
     # Each child can receive at most one gift
     for child_id, child in children.items():
-        problem += lpSum(happy[child_id, factory['id']] for factory
+        problem += lpSum(happy[child_id, factory_id] for factory_id
         in child['factories']) <= 1
 
     # A factory cannot distribute more gifts than it has in stock.
     for factory_id, factory in factories.items():
-        problem += lpSum(happy[child_id, factory_id] for child_id 
+        problem += lpSum(happy[child_id, factory_id] for child_id
         in factory['requests']) <= factory['stock']
 
     for country_id, country in countries.items():
